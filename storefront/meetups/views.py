6e9357635eefcaaf85a70b2a32d508271770f5ef
Paul Lim <paulslim47@gmail.com>
2024-12-04 19:46:07 -0500

    css addition for frontpage

--- storefront/meetups/views.py
+++ storefront/meetups/views.py
@@ -4,8 +4,12 @@ from django.shortcuts import render
 # the path is relative to the teamplates directory already
 def index(request):
     meetups = [
-        { 'title': 'A first meetup'},
-        { 'title': 'A second meetup'},
+        { 'title': 'A first meetup', 
+        'location': 'New York', 
+        'slug': 'a-first-meetup'},
+        { 'title': 'A second meetup', 
+        'location': 'Paris', 
+        'slug': 'a-second-meetup'}
     ]
     return render(request, 'meetups/index.html', {
         'show_meetups': False,
